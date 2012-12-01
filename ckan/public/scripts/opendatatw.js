cd228eff2c2b6fa460eea442cbd47feb15b290a2
Keith Yang <i@keitheis.org>
2012-12-01 17:22:35 +0800

    Grrr… The JAVASCRIPT.

--- ckan/public/scripts/opendatatw.js
+++ ckan/public/scripts/opendatatw.js
@@ -1,6 +1,6 @@
 (function ($) {
-  $('.module-info-overlay').addClass('fade-in');
-  $('section.hero-secondary a.close').click(function() {
+  $('.module-info-overlay').addClass('fade-in')
+  .find('a.close').click(function() {
     $('.module-info-overlay').removeClass('fade-in');
   });
 }(jQuery));
